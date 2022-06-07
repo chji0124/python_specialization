b9c9d8e5cc08fe627190933508a6556713dc979f
jamie choi <chji01242@gmail.com>
2022-06-06 21:08:29 -0400

    Completed Coursera 3

---
+++ Coursera_2/Practice.py
@@ -0,0 +1,21 @@
+import re 
+
+fileHandler = open('/Users/jamie/Desktop/regex_sum_1451275.txt') # opening a file
+
+numbers = list() #creating an empty list
+Sum = 0
+
+for line in fileHandler: # using for loops to iterate over a sequence
+    line = line.rstrip() #stripping white spaces on the right handside
+    nums = re.findall('[0-9]+',line) #to extract matching string for one or more digits 
+    numbers = numbers + nums
+    #print(numbers)
+
+for n in numbers: #used
+    Sum = Sum + int(n)
+
+print(Sum)
+
+
+
+  
---
+++ Coursera_3/assignment/Week2_Assignment.py
@@ -0,0 +1,18 @@
+import re 
+
+fileHandler = open('/Users/jamie/Desktop/regex_sum_1451275.txt') # opening a file
+
+numbers = list() #creating an empty list
+Sum = 0
+
+
+for line in fileHandler: # using for loops to iterate over a sequence
+    line = line.rstrip() #stripping white spaces on the right handside
+    nums = re.findall('[0-9]+',line) #to extract matching string for one or more digits 
+    numbers = numbers + nums #concatting extracted strings to the empty list; tried append() function
+    #print(numbers)
+
+for n in numbers: #used for loop to get sum of the numbers
+    Sum = Sum + int(n)
+
+print(Sum)
